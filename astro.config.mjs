879cf0e19fe55ff7cbd9bc79a30da82221c8566e
maamokun/MikanDev <maamo-kun@outlook.com>
2024-11-05 00:57:46 +0900

    this sucks lets just use cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://neodyland.github.io',
-  base: 'takasumi-web-main',
   integrations: [tailwind(), react()],
 });
